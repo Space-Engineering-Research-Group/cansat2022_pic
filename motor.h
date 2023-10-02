7eb0cb2bc7bf81f01c03dab1bbd80dc0afb8edba
NuskRbb <dpanda.kky@gmail.com>
2023-10-02 23:49:27 +0900

    add: motor.h

---
+++ motor.h
@@ -0,0 +1,9 @@
+#device PIC18F2520
+
+void motor_right_turn(AllPins);
+void motor_left_turn(AllPins);
+void motor_stop(AllPins);
+void motor_forward(AllPins);
+void motor_back(AllPins);
+void motor_right_rotate(AllPins);
+void motor_left_rotate(AllPins);
