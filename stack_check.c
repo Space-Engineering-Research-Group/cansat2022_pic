402a825078775b40e2d09f38f43d9b04d2f68a10
NuskRbb <dpanda.kky@gmail.com>
2023-10-05 22:09:13 +0900

    add: stack_check code

---
+++ stack_check.c
@@ -0,0 +1,61 @@
+#include <math.h>
+#include <calculate.h>
+#include <motor.h>
+#include <stack_check.h>
+#include <get_gps.h>
+
+typedef struct
+{
+    double longitude;
+    double latitude;
+} Coordinate;
+
+void back_check(Coordinate *current, Coordinate *initial, AllPins motorpin){
+    double distance_advanced = distance(current->latitude, current->longitude) - distance(initial->latitude, initial->longitude);
+    if(fabs(distance_advanced) < 0.0005){
+        fprintf(xbee, "Back initial\r\n");
+        motor_back(motorpin);
+        delay_ms(1000);
+    } else {
+        return;
+    }
+    motor_right_rotate(motorpin);
+    delay_ms(1000);
+}
+
+void turn_check(Coordinate *current_coordinate, Coordinate *initial_coordinate, Coordinate *goal_coordinate, AllPins *motor_pin) {
+    Vector *current_vector, *initial_vector, *goal_vector;
+    // TODO: coordinate -> vectortor
+    // calculate inner product
+    // calculate cross product
+
+    current_vector->x = current_coordinate->latitude - initial_coordinate->latitude;
+    current_vector->y = current_coordinate->longitude - initial_coordinate->longitude;
+
+    goal_vector->x = goal_coordinate->latitude - initial_coordinate->latitude;
+    goal_vector->y = goal_coordinate->longitude - initial_coordinate->longitude;
+
+    double to_goal_inner = inner_product(current_vector, goal_vector);
+    double to_goal_cross = cross_product(current_vector, goal_vector);
+
+    // TODO: reference from gps_cal.c
+
+    double to_goal_angle = calculate_angle(to_goal_inner);
+
+    float turn_left_time_per_angle = 1700.00;
+    float turn_right_time_per_angle = 1700.00;
+
+    if(to_goal_cross < 0 && to_goal_angle > 0.05) {
+        int turn_time = turn_left_time_per_angle * to_goal_angle;
+        motor_left_turn(motor_pin);
+        delay_ms(turn_time);
+        motor_forward(motor_pin);
+    } else if(to_goal_cross > 0 && to_goal_angle > 0.05) {
+        int turn_time = turn_left_time_per_angle * to_goal_angle;
+        motor_right_turn(motor_pin);
+        delay_ms(turn_time);
+        motor_forward(motor_pin);
+    }
+
+    return;
+}
---
+++ stack_check.h
@@ -0,0 +1,18 @@
+typedef sturct
+{
+    double longitude;
+    double latitude;
+} Coordinate;
+
+/// @brief function to back up when hitting an obstacle
+/// @param now coordinate of now  
+/// @param start coordinate of start 
+/// @param motor_pin motor pins 
+void back_check(Coordinate, Coordinate, AllPins);
+
+/// @brief function to correct and proceed if the line to the goal is misaligned
+/// @param now_coordinate Current coordinate
+/// @param start_coordinate initial coordinate 
+/// @param goal_coordinate goal coordinate
+/// @param motor_pin motor pins
+void turn_check(Coordinate, Coordinate, Coordinate, AllPins);
