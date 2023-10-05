bd5317470d516e7ad23b55c604aee33d1b941d7e
NuskRbb <dpanda.kky@gmail.com>
2023-10-05 22:14:29 +0900

    feat: add comment

--- motor.h
+++ motor.h
@@ -12,10 +12,30 @@ typedef struct
    MotorPin Left;
 } AllPins;
 
+/// @brief turn right
+/// @param motor_pin motor pins 
 void motor_right_turn(AllPins);
+
+/// @brief turn left
+/// @param motor_pin motor pins 
 void motor_left_turn(AllPins);
+
+/// @brief stop
+/// @param motor_pin motor pins 
 void motor_stop(AllPins);
+
+/// @brief forward
+/// @param motor_pin motor pins 
 void motor_forward(AllPins);
+
+/// @brief back
+/// @param motor_pin motor pins 
 void motor_back(AllPins);
+
+/// @brief right rotate
+/// @param motor_pin motor pins 
 void motor_right_rotate(AllPins);
+
+/// @brief left rotate
+/// @param motor_pin motor pins 
 void motor_left_rotate(AllPins);
